3c581cb32fcc2ba91669fb4e1000227dc4fc949a
GTUNE2-01\ktc <>
2025-06-04 12:24:37 +0900

    ⚡️ Navをコンポーネント化した

--- components/mainheader/page.tsx
+++ components/mainheader/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { motion } from "framer-motion";
+import NavItem from "./navitems";
 import Link from "next/link";
 
 export default function MainHeader() {
@@ -17,34 +17,10 @@ export default function MainHeader() {
 
                     {/* デスクトップナビゲーション - md以上で表示 */}
                     <nav className="flex items-center space-x-6">
-                        <motion.div className="relative inline-block" whileHover="hover" initial="rest" animate="rest">
-                            <Link href="/profile" className="font-medium text-lime-900 hover:text-gray-800 transition-colors duration-300">
-                                Profile
-                            </Link>
-                            <motion.span
-                                variants={{
-                                    rest: { opacity: 0, width: 0 },
-                                    hover: { opacity: 1, width: "100%" }
-                                }}
-                                transition={{ duration: 0.3 }}
-                                className="block h-1 bg-lime-900 rounded absolute left-0 -bottom-1"
-                                style={{ pointerEvents: "none" }}
-                            />
-                        </motion.div>
-                        <motion.div className="relative inline-block" whileHover="hover" initial="rest" animate="rest">
-                            <span className="font-medium text-lime-900 hover:text-gray-800 transition-colors duration-300">
-                                Blog(Creating...)
-                            </span>
-                            <motion.span
-                                variants={{
-                                    rest: { opacity: 0, width: 0 },
-                                    hover: { opacity: 1, width: "100%" }
-                                }}
-                                transition={{ duration: 0.3 }}
-                                className="block h-1 bg-lime-900 rounded absolute left-0 -bottom-1"
-                                style={{ pointerEvents: "none" }}
-                            />
-                        </motion.div>
+                        <NavItem href="/profile" label="Profile" />
+                        <NavItem href="/blog" label="Blog(工事中)" disabled/>
+                        <NavItem href="/works" label="Works(工事中)" disabled/>
+                        <NavItem href="/contact" label="Contact(工事中)" />
                     </nav>
                 </div>
             </div>
